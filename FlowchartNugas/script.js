aa465f62dfe8e0567908ae9d3545d2b039c4f6c7
Pal? <99118944+NauvalAqiilah@users.noreply.github.com>
2023-11-07 14:48:58 +0700

    Add files via upload

---
+++ FlowchartNugas/script.js
@@ -0,0 +1,8 @@
+let Nilai = prompt("Masukan Nilai anda: ");
+if(Nilai>= 80 && Nilai<=100){
+    alert("Anda lulus")
+    document.write("selamat")
+}else{
+    alert("anda remed")
+    document.write("Coba perbaiki lagi")
+}
